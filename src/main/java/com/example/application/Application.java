d2dd2907611e68f262fa91ec9d0b702ea96af78d
Syabana Tri Wahyudi <syabana.wahyudi@gmail.com>
2026-08-25 14:11:27 +0700

    add keycloak docker

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,11 +1,17 @@
 package com.example.application;
 
+import com.example.application.data.User;
+import com.example.application.data.UserRepository;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
@@ -20,8 +26,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @StyleSheet("styles.css")
 @Theme(value = "my-app")
 public class Application implements AppShellConfigurator {
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+   
 }
